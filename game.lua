8299e83988cc496661f15af3d539c5527b6d6e7b
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-01-30 21:08:25 -0300

    Update game.lua

--- game.lua
+++ game.lua
@@ -1,4 +1,9 @@
 function update()
+  ui.palset(0, 0x03E6) 
   ui.palset(1, 0x07E0) 
-  ui.print("hello world", 20, 20. 1)
+  
+  ui.clip(0, 0, 480, 270)
+  ui.camera()
+  
+  ui.print("hello world", 20, 20, 1)
 end 
