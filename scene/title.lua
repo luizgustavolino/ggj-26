c3bed95bbf60e34d325409c2ef0d5f1b5236e859
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-02-01 02:12:31 -0300

    .

--- scene/title.lua
+++ scene/title.lua
@@ -1,17 +1,30 @@
 local M = {}
 
+local TitleStates = {
+    waiting_start = "waiting_start",
+    waiting_players = "waiting_players",
+    transition_to_game = "transition_to_game"
+}
+
 M.init = function()
 
 end 
 
 M.update = function(frame)
     if ui.btn(BTN_Z, 0) then
-        ui.tile(Sprites.img.nplayers, 0, 480/2 - 32, 220)
+        
     end
 end 
 
 M.draw = function(frame)
-    ui.spr(Sprites.img.title, 480/2 - 171/2, 40)
+    ui.spr(Sprites.img.title, 480/2 - 170/2, 40)
+
+    if M.state == TitleStates.waiting_start then
+        ui.print("Aperte A ou B para iniciar", 480/2 - 128/2, 200, 181)
+    elseif M.state == TitleStates.waiting_players then
+        ui.tile(Sprites.img.nplayers, 0, 480/2 - 32 - 64, 220)
+        ui.tile(Sprites.img.nplayers, 1, 480/2 - 32 + 64, 220)
+    end
 end 
 
 return M
