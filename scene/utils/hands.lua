d0c73145e86d66fd1058bd4295bde12b45230b2c
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-02-01 11:09:58 -0300

    .

--- scene/utils/hands.lua
+++ scene/utils/hands.lua
@@ -16,7 +16,7 @@ local function new()
 
     local drawers = {
         [HandStates.waiting] = function(frame)
-            ui.spr(Sprites.img["observe"..(frame//10)], 480/2 - 128/2, 270/2 - 32/2)
+            ui.spr(Sprites.img["observe"..(1 + (frame//10)%10)], 480/2 - 128/2, 270/2 - 32/2)
         end,
         [HandStates.playing] = function(frame)
             local d = 0
