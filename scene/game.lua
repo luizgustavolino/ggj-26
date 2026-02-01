bd88d40aa4007823a411b2fb3dd12b0b0107af1d
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-02-01 14:18:07 -0300

    .

--- scene/game.lua
+++ scene/game.lua
@@ -140,6 +140,10 @@ M.draw = function(frame)
         else 
             ui.spr(Sprites.img["perdeu" .. f], 480/2 - 128/2, 4)
         end 
+
+        if ui.btnp(BTN_Q, 0) || ui.btnp(BTN_E, 0) then
+             Director.change_scene("menu")
+        end 
     end
     
     M.state_frame = M.state_frame + 1
--- scene/utils/ninja.lua
+++ scene/utils/ninja.lua
@@ -200,11 +200,12 @@ local function new()
         end,
         [NinjaStates.smoke] = function(frame)
             local f = ((frame / 9)) // 1
-            if f < 9 then
+            if f < 10 then
                 if f > 3 then
                     ui.tile(Sprites.map.scene_b, item_index, M.x, M.y)          
                 end
-                ui.tile(Sprites.img.ninja_a, 3 + f, M.x, M.y)
+                local m = math.min(11, 3 + f)
+                ui.tile(Sprites.img.ninja_a, m, M.x, M.y)
             end
         end,
         [NinjaStates.freeze] = function(frame, player)
@@ -274,7 +275,7 @@ local function new()
 
                 M.x = M.tile_x * TILE_SIZE
                 M.y = M.tile_y * TILE_SIZE
-                
+
                 M.change_state(NinjaStates.freeze)
             end,
             [GameStates.level_conclusion] = function()
