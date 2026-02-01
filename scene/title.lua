3eb707bdcf77f5bdd91ca0876a7206cfcb10bb37
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-02-01 01:53:55 -0300

    .

--- scene/title.lua
+++ scene/title.lua
@@ -5,15 +5,13 @@ M.init = function()
 end 
 
 M.update = function(frame)
-    if ui.btnp(BTN_Z, 0) and frame > 3 then
-        sfx.fx(60, 20)
+    if ui.btn(BTN_Z, 0) then
+        ui.spr(Sprites.img.nplayers, 480/2 - 32, 120)
     end
 end 
 
 M.draw = function(frame)
     ui.spr(Sprites.img.title, 480/2 - 180/2, 40)
-
-
 end 
 
 return M
