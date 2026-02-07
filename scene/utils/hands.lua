df3aca7c03ca780db55cbb4b01c457d6cfc86e03
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2026-02-07 09:41:35 -0300

    .

--- scene/utils/hands.lua
+++ scene/utils/hands.lua
@@ -131,7 +131,7 @@ local function new()
             sfx.fx(34, 60)
         else 
             table.insert(M.bets, {x = x, y = y})
-            sfx.fx(11, 50)
+            sfx.fx(62, 55)
         end 
     end 
 
